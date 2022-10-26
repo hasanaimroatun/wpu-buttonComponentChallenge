1dd048fb1c8edc1eeebfbadd52f6c8c219224c7a
hasanaimroatun <imroatunhasana@gmail.com>
2022-10-26 10:20:11 +0700

    create file, add elements in template, and export file

---
+++ src/components/Navigation.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div class="mb-5"><b><span>Dev</span>challenges.io</b></div>
+        <div class="navBar">
+            <ul class="navbar-nav pe-3">
+                <li class="nav-item mb-3">
+                    <a class="nav-link" href="#">Colors</a>
+                </li>
+                <li class="nav-item mb-3">
+                    <a class="nav-link" href="#">Typography</a>
+                </li>
+                <li class="nav-item mb-3">
+                    <a class="nav-link" href="#">Spaces</a>
+                </li>
+                <li class="nav-item mb-3">
+                    <a class="nav-link" href="#">Buttons</a>
+                </li>
+                <li class="nav-item mb-3">
+                    <a class="nav-link" href="#"><b>Inputs</b></a>
+                </li>
+                <li class="nav-item mb-3">
+                    <a class="nav-link" href="#">Grid</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Navigation-bar'
+    }
+</script>
+
+<style scoped>
+
+</style>
