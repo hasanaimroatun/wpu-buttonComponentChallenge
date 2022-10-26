a1e45074b3a42ee356d86b1261450b7e0b0819e0
hasanaimroatun <imroatunhasana@gmail.com>
2022-10-26 09:44:23 +0700

    create file, add element in template, export file and add data in script

---
+++ src/components/ButtonDefault.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="row mt-2 mb-5">
+        <div class="col-4">
+            <div v-text="label1"></div>
+            <button type="button" class="btn btn-secondary mt-1">Default</button>
+        </div>
+        <div class="col">
+            <div v-text="label2"></div>
+            <button type="button" class="btn btn-secondary mt-1">Default</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Button-default',
+        data() {
+            return {
+                label1: '<Button />',
+                label2: '&:hover, &:focus'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
