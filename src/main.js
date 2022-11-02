b6da7a2840e4071336c57b9b6df130197f7d3580
hasanaimroatun <imroatunhasana@gmail.com>
2022-11-02 18:36:47 +0700

    import icon from fontawesome, import dom rom fontawesome, add icon to library, and add dom.watch( )

--- src/main.js
+++ src/main.js
@@ -4,7 +4,15 @@ import "../node_modules/bootstrap/dist/css/bootstrap.css"
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle"
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
+import { faCartPlus, faPaperPlane, faTrash, faThumbsUp, faUpload, faDownload } from '@fortawesome/free-solid-svg-icons'
+import { dom } from '@fortawesome/fontawesome-svg-core'
+
 library.add(faCartPlus)
+library.add(faPaperPlane)
+library.add(faTrash)
+library.add(faThumbsUp)
+library.add(faUpload)
+library.add(faDownload)
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+dom.watch()
